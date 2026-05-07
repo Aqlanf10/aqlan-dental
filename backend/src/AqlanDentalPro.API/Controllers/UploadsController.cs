8d4e6f8b9f4dce27390db5866a5959e9bf853918
Z User <z@container>
2026-05-07 13:04:10 +0000

    fix: use System.IO.File to avoid ControllerBase.File() conflict

--- backend/src/AqlanDentalPro.API/Controllers/UploadsController.cs
+++ backend/src/AqlanDentalPro.API/Controllers/UploadsController.cs
@@ -33,8 +33,8 @@ public class UploadsController : ControllerBase
             Directory.CreateDirectory(primaryPath);
             // Test write permission
             var testFile = Path.Combine(primaryPath, $".write-test-{Guid.NewGuid()}");
-            File.WriteAllText(testFile, "test");
-            File.Delete(testFile);
+            System.IO.File.WriteAllText(testFile, "test");
+            System.IO.File.Delete(testFile);
             return primaryPath;
         }
         catch
